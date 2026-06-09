cb2d3d325525340042ebdb84ba673d3bfb456d63
zshgustavo <zshgustavo@gmail.com>
2026-06-09 11:01:57 -0300

    add img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -59,10 +59,10 @@ Use markdown para formatar seu texto com **negrito**, *itálico*, e muito mais.`
                 }}
               />
             </div>
-            <div className="about-image-wrapper">
+            <div className="about-image-wrapper about-image-wrapper--transparent">
               <img
-                src="/images/about-2.png"
-                alt={t('about.title')}
+                src="https://res.cloudinary.com/dawnv7igk/image/upload/w_800,q_auto,f_auto/Design_sem_nome_gpkg6f.png"
+                alt="Certificações"
                 className="about-photo"
                 onError={(e) => {
                   e.target.style.display = 'none'
